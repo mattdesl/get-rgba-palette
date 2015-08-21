7a64b5ea837b7d58caa91db68fd768946bfc1814
mattdesl <dave.des@gmail.com>
2015-08-21 17:38:50 -0400

    add a filter function

--- test.js
+++ test.js
@@ -4,16 +4,29 @@ var array = require('array-range')
 var almostEqual = require('almost-equal')
 
 var RED = [255,0,0,255]
+var BLUE_TRANSPARENT = [0,0,255,0]
+var concat = function(a, b) { return a.concat(b) }
 
 test("gets a palette of main colors from an array of pixels", function(t) {
     //a red image 
     var red = array(100)
         .map(function() { return RED })
-        .reduce(function(a, b) { return a.concat(b) })
+        .reduce(concat)
     
     t.deepEqual(palette(red, 1), [ [252,4,4] ], 'should guess red-like rgba color')
     t.deepEqual(palette(red, 0), [], 'should return empty array')
     t.deepEqual(palette(red, 6).length, 6, 'should return 6 colors')
+    
+    function truthy() {
+        return true
+    }
+    var blue = array(300)
+        .map(function () { return BLUE_TRANSPARENT })
+        .reduce(concat)
+    var redAndBlue = red.slice().concat(blue)
+        
+    t.deepEqual(palette(redAndBlue, 1, undefined, undefined), [ [ 252, 4, 4 ] ], 'gets red')
+    t.deepEqual(palette(redAndBlue, 1, undefined, truthy), [ [ 4, 4, 252 ] ], 'gets blue')
     
     var gradient = array(100)
         .map(function(i, x, self) { 
